b37f11dcec11d1dcda5f725a1b2b6e88637bd6e4
UMANG LOTIYA <lotiya8@gmail.com>
2019-06-11 13:15:38 +0530

    Initial Commit

---
+++ aether_news/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  aether_news
+//
+//  Created by UMANG LOTIYA on 11/06/19.
+//  Copyright © 2019 Neeti-i beyond private limited. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
